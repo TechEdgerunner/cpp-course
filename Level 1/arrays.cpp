0336d9d86495aa7db98e5215a79d1ff57d5944e7
TechEdgerunner <maycon.bruno.bez@gmail.com>
2024-07-03 08:33:07 -0300

    changes in launch and tasks, add arrays and const

---
+++ Level 1/arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    double temperature_list[4] = {98.6, 99.1, 99.0, 98.7};  // Array of 4 temperatures
+
+    std::cout<<"Temperature 1: "<<temperature_list[0]<<"\n";
+    std::cout<<"Temperature 2: "<<temperature_list[1]<<"\n";
+    std::cout<<"Temperature 3: "<<temperature_list[2]<<"\n";
+    std::cout<<"Temperature 4: "<<temperature_list[3]<<"\n";
+
+    return 0;
+}
---
+++ Level 1/const.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main(){
+
+    const double max_allowed_temperature = 76.8;
+
+    // max_allowed_temperature = 80.0; // This will give an error
+
+    std::cout << "Max allowed temperature: " << max_allowed_temperature << std::endl;
+
+    return 0;
+}
