0cc986ba9870e131e481779c8c87575c39471da2
TechEdgerunner <maycon.bruno.bez@gmail.com>
2024-07-08 21:27:13 -0300

    started lvl3

---
+++ Level 3/bool.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+//    cout<<true<<endl; // 1
+//    cout<<false<<endl; // 0
+
+
+//    cout<<boolalpha; // true, false
+//    cout<<true<<endl; // true
+//    cout<<false<<endl; // false
+
+
+    cout<<boolalpha;
+    cout<< (1 == 1) << endl; // true
+    cout<< (1 == 2) << endl; // false
+
+
+    return 0;
+}
